eb8b8b3970517b78da3df9fdc1f6a23f617ade83
Sacha Greif <sacha357@gmail.com>
2014-06-04 11:09:56 +0900

    improved animations

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -1,6 +1,25 @@
+showNewsletter = true
+
 $(function(){
   // $(".video").fitVids();
   $('.sidebar-toggle').click(function(){
     $('body').toggleClass('show-sidebar');
   });
+
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop() + $(window).height();
+    var height = $(document).height();
+    var percent = Math.round(scroll * 100 / height);
+    console.log(scroll, height, percent)
+    if(percent > 70 && showNewsletter == true){
+      $('body').addClass('show-newsletter');
+    }else{
+      $('body').removeClass('show-newsletter');     
+    }
+  });
+  $('.newsletter-dismiss').click(function(){
+    $('body').removeClass('show-newsletter');  
+    showNewsletter = false;
+  });
+
 });
